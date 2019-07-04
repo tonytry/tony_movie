2f15d5c77a7283bff00101274a2a41cc34c7a996
tonytry <wenlong.1988@hotmail.com>
2019-07-04 11:10:18 +0800

    add setData

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -4,88 +4,110 @@
       <div class="city_hot">
         <h2>热门城市</h2>
         <ul class="clearfix">
-          <li>上海</li>
-          <li>北京</li>
-          <li>上海</li>
-          <li>北京</li>
-          <li>上海</li>
-          <li>北京</li>
-          <li>上海</li>
-          <li>北京</li>
+          <li v-for="item in hotList" :key="item.id"> {{item.nm}} </li>
         </ul>
       </div>
-      <div class="city_sort">
-        <div>
-          <h2>A</h2>
+      <!-- 添加ref属性，使在点击handleToIndex 这个功能时，可以进行位置的定位 -->
+      <div class="city_sort" ref="city_sort">
+        <div v-for="item in cityList" :key="item.index">
+          <h2> {{item.index}} </h2>
           <ul>
-            <li>阿拉善盟</li>
-            <li>鞍山</li>
-            <li>安庆</li>
-            <li>安阳</li>
-          </ul>
-        </div>
-        <div>
-          <h2>B</h2>
-          <ul>
-            <li>北京</li>
-            <li>保定</li>
-            <li>蚌埠</li>
-            <li>包头</li>
-          </ul>
-        </div>
-        <div>
-          <h2>A</h2>
-          <ul>
-            <li>阿拉善盟</li>
-            <li>鞍山</li>
-            <li>安庆</li>
-            <li>安阳</li>
-          </ul>
-        </div>
-        <div>
-          <h2>B</h2>
-          <ul>
-            <li>北京</li>
-            <li>保定</li>
-            <li>蚌埠</li>
-            <li>包头</li>
-          </ul>
-        </div>
-        <div>
-          <h2>A</h2>
-          <ul>
-            <li>阿拉善盟</li>
-            <li>鞍山</li>
-            <li>安庆</li>
-            <li>安阳</li>
-          </ul>
-        </div>
-        <div>
-          <h2>B</h2>
-          <ul>
-            <li>北京</li>
-            <li>保定</li>
-            <li>蚌埠</li>
-            <li>包头</li>
+            <li v-for="itemList in item.list" :key="itemList.id"> {{itemList.nm}} </li>
           </ul>
         </div>
       </div>
     </div>
-    <div class="city_index">
+
+     <div class="city_index">
       <ul>
-        <li>A</li>
-        <li>B</li>
-        <li>C</li>
-        <li>D</li>
-        <li>E</li>
+        <!-- 这边的index 是(item, index) in cityList 的index 位置 -->
+        <li v-for="(item, index) in cityList" :key="item.index" @touchstart = "handleToIndex (index)"> {{item.index}} </li>
       </ul>
     </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'City'
+  name: 'City',
+  // 创建数据空实例
+  data () {
+    return {
+      cityList: [],
+      hotList: []
+    }
+  },
+  // 引入api数据
+  mounted () {
+    this.axios.get('/api/cityList').then((res) => {
+      var msg = res.data.msg
+      if (msg === 'ok') {
+        var cities = res.data.data.cities
+        // [ { index: 'A', list: [ { nm: '安徽', id= 123 }]}] 这种形式
+        var { cityList, hotList } = this.formatCityList(cities)
+        // 进行映射
+        this.cityList = cityList
+        this.hotList = hotList
+      }
+    })
+  },
+  methods: {
+    formatCityList (cities) {
+      // 创建一个初始化的结果集
+      var cityList = []
+      var hotList = []
+      // 进行循环选择出热门城市
+      for (var i = 0; i < cities.length; i++) {
+        if (cities[i].isHot === 1) {
+          hotList.push(cities[i])
+        }
+      }
+      // 循环cities这个数组
+      // eslint-disable-next-line no-redeclare
+      for (var i = 0; i < cities.length; i++) {
+        var firstLetter = cities[i].py.substring(0, 1).toUpperCase()
+        if (toCom(firstLetter)) { // 新添加索引
+          cityList.push({ index: firstLetter, list: [{ nm: cities[i].nm, id: cities[i].id }] })
+        } else { // 累加已有索引
+          for (var j = 0; j < cityList.length; j++) {
+            if (cityList[j].index === firstLetter) {
+              cityList[j].list.push({ nm: cities[i].nm, id: cities[i].id })
+            }
+          }
+        }
+      }
+      // cityList 进行有序排列
+      cityList.sort((n1, n2) => {
+        if (n1.index > n2.index) {
+          return 1
+        } else if (n1.index < n2.index) {
+          return -1
+        } else {
+          return 0
+        }
+      })
+
+      function toCom (firstLetter) {
+        for (var i = 0; i < cityList.length; i++) {
+          if (cityList[i].index === firstLetter) {
+            return false
+          }
+        }
+        return true
+      }
+      return {
+        cityList,
+        hotList
+      }
+    },
+    handleToIndex (index) {
+      // 转跳到对应的h2位置上
+      var h2 = this.$refs.city_sort.getElementsByTagName('h2')
+      // 这边是让citysort父元素滚动到h2的位置
+      this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routers'
 import store from './stores'
+// 这样引入的好处是，在各个组件中只要通过this.axios就可以直接引用了
+import axios from 'axios'
+Vue.prototype.axios = axios
+
+// 全局对照片w h过滤器 对url字段中的w.h进行替换，这边用/ /， 当转换实际.用 \
+Vue.filter('setWH', (url, arg) => {
+  return url.replace(/w\.h/, arg)
+})
 
 Vue.config.productionTip = false
 
